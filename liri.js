7f84bbb534f165a4f7a6ac2aa769caf36e4d2718
CBrown <47484591+caycebrown@users.noreply.github.com>
2019-05-06 09:59:10 -0500

    updated initial prompt to allow user to choose which api function to interact with

--- liri.js
+++ liri.js
@@ -39,7 +39,7 @@ try{
     .prompt([
       {
         type: "input",
-        message: "Enter the name of the song you would like to search for",
+        message: "Enter the name of the song you would like to search for: ",
         name: "song"
       }
     ]).then(response => {
@@ -50,6 +50,9 @@ try{
 
       spotify.search({ type: 'track', query: song, limit: 1 }, function(err, data) {
           if (err) return console.log('Error occurred: ' + err);
+
+        console.log('\nYour Requested Data');
+        console.log('==================================================')
         console.log('\nAlbum: ' + data.tracks.items[0].album.name);
         console.log('Artist: ' + data.tracks.items[0].artists[0].name);
         console.log('Track Name: ' + data.tracks.items[0].name);
@@ -57,7 +60,7 @@ try{
         });
     });
 
-}catch(err){console.log(err)};
+}catch(err){console.log('There was an error 1: ' + err)};
 };
 
 
@@ -68,23 +71,39 @@ try{
 //===================================================================================================
 function movieThis(){
 
-  var title = process.argv[3];
+try{
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter the name of the movie you would like to search for: ",
+        name: "title"
+      }
+    ]).then(response => {
+      
+      var title = response.title;
 
-  axios.get(`http://www.omdbapi.com/?t=${title}&y=&plot=short&apikey=trilogy`).then(
-    function(response) {
-      console.log('\nTitle: ' + response.data.Title);
-      console.log('Year of Release: ' + response.data.Year);
-      console.log('IMDB Rating: ' + response.data.imdbRating);
-      console.log('Rotten Tomatoes Rating: ' + response.data.Ratings[1].Value);
-      console.log('Country: ' + response.data.Country);
-      console.log('Language: ' + response.data.Language);
-      console.log('Plot: ' + response.data.Plot);
-      console.log('Actors: ' + response.data.Actors);
-    }
-  );
+      axios.get(`http://www.omdbapi.com/?t=${title}&y=&plot=short&apikey=trilogy`).then(
+        function(response) {
+          
+          console.log('\nYour Requested Data');
+          console.log('==================================================')
+          console.log('\nTitle: ' + response.data.Title);
+          console.log('Year of Release: ' + response.data.Year);
+          console.log('IMDB Rating: ' + response.data.imdbRating);
+          console.log('Rotten Tomatoes Rating: ' + response.data.Ratings[1].Value);
+          console.log('Country: ' + response.data.Country);
+          console.log('Language: ' + response.data.Language);
+          console.log('Plot: ' + response.data.Plot);
+          console.log('Actors: ' + response.data.Actors);
+        }
+      );
+    })
+
+
+}catch(error){console.log("There was an error2: " + error)}
 
 };
-
 
 
 
@@ -93,14 +112,37 @@ function movieThis(){
 //===================================================================================================
 function concertThis(){
 
-  var artist = process.argv[4];
+try{
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter the name of the artist you would like to search for: ",
+        name: "artist"
+      }
+    ]).then(response => {
 
-  axios.get(`https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`).then(
-    function(response) {
-      console.log('\n'+ response.data[0].venue.name);
-      console.log(response.data[0].venue.city + ', ' + response.data[0].venue.country);
-      console.log(response.data[0].datetime);
-    }
-  );
+      var artist = response.artist;
 
+      axios.get(`https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`).then(
+        function(response) {
+          
+          console.log('\nYour Requested Data');
+          console.log('==================================================')
+
+          //Function for making upcoming concert info blocks
+          function makeData(response){
+
+            console.log('\n'+ response.venue.name);
+            console.log(response.venue.city + ', ' + response.venue.country);
+            console.log(response.datetime);
+
+          }
+          //looping through all concert objects and passing them into the function above
+          response.data.map(makeData);
+        }
+      );
+    });
+
+}catch(error){console.log('There was an error3: ' + error)}
 };
